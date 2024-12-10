a51300a1de5ad21260f93622f004061255067054
Constantin Chirila <constantin.chirila@gmail.com>
2024-12-10 11:34:36 +0000

    Fixed the node demo to properly handle the errors

--- node/app.js
+++ node/app.js
@@ -11,41 +11,42 @@
 // limitations under the License.
 //
 
-const express = require('express');
-const bodyParser = require('body-parser');
-require('isomorphic-fetch');
-const { DaprClient, CommunicationProtocolEnum } = require('@dapr/dapr');
+const express = require("express");
+const bodyParser = require("body-parser");
+require("isomorphic-fetch");
+const { DaprClient, CommunicationProtocolEnum } = require("@dapr/dapr");
 
-const communicationProtocol = (process.env.DAPR_PROTOCOL === "grpc")
+const communicationProtocol =
+  process.env.DAPR_PROTOCOL === "grpc"
     ? CommunicationProtocolEnum.GRPC
-    : CommunicationProtocolEnum.HTTP
+    : CommunicationProtocolEnum.HTTP;
 
-const daprHost = process.env.DAPR_HOST ?? "localhost"
-
-let daprPort
-switch (communicationProtocol) {
-    case CommunicationProtocolEnum.HTTP: {
-        daprPort = process.env.DAPR_HTTP_PORT
-        break
-    }
-    case CommunicationProtocolEnum.GRPC: {
-        daprPort = process.env.DAPR_GRPC_PORT
-        break
-    }
-    default: {
-        port = 3500
-    }
-}
+const daprHost = process.env.DAPR_HOST ?? "localhost";
 
 const stateStoreName = process.env.STATE_STORE_NAME ?? "statestore-und";
 const queryStateStoreName = process.env.STATE_STORE_NAME ?? "statestore-im";
 const pubSubName = process.env.PUBSUB_NAME ?? "pubsub";
-const port = process.env.APP_PORT ?? "3000";
+let port = process.env.APP_PORT ?? "4000";
+
+let daprPort;
+switch (communicationProtocol) {
+  case CommunicationProtocolEnum.HTTP: {
+    daprPort = process.env.DAPR_HTTP_PORT;
+    break;
+  }
+  case CommunicationProtocolEnum.GRPC: {
+    daprPort = process.env.DAPR_GRPC_PORT;
+    break;
+  }
+  default: {
+    port = 3500;
+  }
+}
 
 const daprClient = new DaprClient(
-    daprHost, // Dapr sidecar address
-    daprPort,    // Dapr HTTP port
-    communicationProtocol
+  daprHost, // Dapr sidecar address
+  daprPort, // Dapr HTTP port
+  communicationProtocol
 );
 
 const app = express();
@@ -53,39 +54,42 @@ app.use(bodyParser.json());
 //app.use(express.json());
 
 // Get state in Dapr
-app.get('/order/:orderId', async (req, res) => {
+app.get("/order/:orderId", async (req, res) => {
   try {
     console.log(`Getting state with client, order id: ${req.params.orderId}`);
-    const response = await daprClient.state.get(stateStoreName, req.params.orderId);
+    const response = await daprClient.state.get(
+      stateStoreName,
+      req.params.orderId
+    );
     console.log(`Response: ${JSON.stringify(response)}`);
     return res.send(response);
   } catch (error) {
-    console.log("Error caught is: " + error);
-    var json = error.toString().replace("Error: ", "");
-    json = JSON.parse(json);
-    return res.status(500).send(json);
+    console.error("Error caught is: " + error.message);
+    return res.status(500).send(error.message);
   }
 });
 
 // Dapr Binding
-app.post('/binding', async (req, res) => {
+app.post("/binding", async (req, res) => {
   const bindingOperation = "create";
   const { bindingName, message } = req.body;
 
   try {
-    const response = await daprClient.binding.send(bindingName, bindingOperation, message);
+    const response = await daprClient.binding.send(
+      bindingName,
+      bindingOperation,
+      message
+    );
     console.log(`Response: ${JSON.stringify(response)}`);
     return res.send(`Binding request sent ${response}`);
   } catch (error) {
-    console.log("Error caught is: " + error);
-    var json = error.toString().replace("Error: ", "");
-    json = JSON.parse(json);
-    return res.status(500).send(json);
+    console.error("Error caught is: " + error.message);
+    return res.status(500).send(error.message);
   }
 });
 
 // Querying state in Dapr
-app.post('/query', async (req, res) => {
+app.post("/query", async (req, res) => {
   const { query } = req.body;
   try {
     console.log(`Querying state store: ${query}`);
@@ -93,45 +97,40 @@ app.post('/query', async (req, res) => {
     console.log(`Response: ${JSON.stringify(response)}`);
     return res.send(`Queried state store: ${query}`);
   } catch (error) {
-    console.log("Error caught is: " + error);
-    var json = error.toString().replace("Error: ", "");
-    json = JSON.parse(json);
-    return res.status(500).send(json);
+    console.error("Error caught is: " + error.message);
+    return res.status(500).send(error.message);
   }
 });
 
-// Dapr Publish/Subscribe 
-app.post('/publish', async (req, res) => {
+// Dapr Publish/Subscribe
+app.post("/publish", async (req, res) => {
   const { topic, message } = req.body;
-  try {
-    console.log(`Publishing to topic: ${topic}`);
-    const response = await daprClient.pubsub.publish(pubSubName, topic, message);
-    console.log(`Response: ${JSON.stringify(response)}`);
-    
-    if (response.error){
-      throw `Error publishing message to topic: ${topic}`;
-    }
-    return res.send(`Message published to ${topic}`);
-  } catch (error) {
-    return res.status(500).send(error);
+
+  console.log(`Publishing to topic: ${topic}`);
+  const response = await daprClient.pubsub.publish(pubSubName, topic, message);
+  console.log(`Response: ${JSON.stringify(response)}`);
+
+  if (response.error) {
+    console.error("Error response:", response.error);
+    return res.status(500).send(response.error.message);
   }
+  return res.send(`Message published to ${topic}`);
 });
 
-// // Save state in Dapr
-// app.post('/order', async (req, res) => {
-//   const { key, value } = req.body;
-//   try {
-//     console.log(`Saving state, order id: ${key}`);
-//     const response = await daprClient.state.save(stateStoreName, [{ key, value }]);
-//     console.log(`Response: ${JSON.stringify(response)}`);
-//     if (response.error){
-//       throw `Error saving state with key: ${key}`;
-//     }
-//     return res.send(`State saved with key: ${key}`);
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).send(error);
-//   }
-// });
+// Save state in Dapr
+app.post("/order", async (req, res) => {
+  const { key, value } = req.body;
+
+  console.log(`Saving state, order id: ${key}`);
+  const response = await daprClient.state.save(stateStoreName, [
+    { key, value },
+  ]);
+  console.log(`Response: ${JSON.stringify(response)}`);
+  if (response.error) {
+    console.error("Error response:", response.error);
+    return res.status(500).send(response.error.message);
+  }
+  return res.send(`State saved with key: ${key}`);
+});
 
 app.listen(port, () => console.log(`Node App listening on port ${port}!`));
